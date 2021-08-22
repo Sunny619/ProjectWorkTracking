692d495b563cb7675354f9100356587f7a38e192
uditnm <udit20001@gmail.com>
2021-08-22 22:43:44 +0530

    added save option for SharedPreferences

--- app/src/main/java/com/example/ProjectWorkTracking/MainActivity.java
+++ app/src/main/java/com/example/ProjectWorkTracking/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,24 +27,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(Projects == null)
-            Projects = new ArrayList<>();
-//        Boolean[] a = new Boolean[3];
-//        a[0] = true;
-//        a[1] = true;
-//        a[2] = true;
-//        Projects.add(new Project("2","2","2","2","2", 2,3, a, false ));
-//        Projects.add(new Project("2","2","2","2","2", 2,3, a, false ));
-//        Projects.add(new Project("2","2","2","2","2", 2,3, a, false ));
+
+        loadData();
+        buildRecyclerView();
+
+
+        Button buttonSave = findViewById(R.id.button_save);
+//        buttonSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                saveData();
+//            }
+//        });
+
+
+
+
+
+    }
+
+    private void buildRecyclerView() {
         projectsRecView = findViewById(R.id.Recycle);
         ProjectsRecViewAdapter adapter = new ProjectsRecViewAdapter();
         adapter.setProjects(Projects);
         projectsRecView.setAdapter(adapter);
         projectsRecView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<Project>>() {}.getType();
+        Projects = gson.fromJson(json, type);
+
+        if (Projects == null) {
+            Projects = new ArrayList<>();
+        }
+    }
+
     public void addNewProject(View view) {
         Intent intent = new Intent(this, AddNewProjectActivity.class);
         startActivity(intent);
     }
     //TODO: create functionality for Add new Project button
+
+
+
 }
